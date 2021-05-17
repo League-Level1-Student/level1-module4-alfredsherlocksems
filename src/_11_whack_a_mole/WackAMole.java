e19b0b4fccc937982cfd8ab0cafeea39299a99b5
student <student@ip-172-31-59-232.us-west-2.compute.internal>
2021-05-17 13:26:45 -0700

    Cool stuff.

---
+++ src/_11_whack_a_mole/WackAMole.java
@@ -0,0 +1,51 @@
+package _11_whack_a_mole;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class WackAMole implements ActionListener {
+	public JFrame frame = new JFrame();
+	public JPanel panel = new JPanel();
+	JButton moleButton;
+	
+	public void start () {
+		frame.setVisible(true);
+		frame.setTitle("Wack a Mole!");
+		frame.setSize(200, 200);
+		frame.add(panel);
+		panel.setLayout(new GridLayout(3, 4));
+		Random ran = new Random();
+		DrawButtons(ran.nextInt(12));
+	}
+	void DrawButtons (int num) {
+		for (int i = 0; i < 12; i++) {
+			JButton button = new JButton();	
+			panel.add(button);
+			button.addActionListener(this);
+			if (num == i) {
+				moleButton = button;
+				moleButton.setText("mole");
+			}
+		} 
+		frame.pack();	
+    }
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		if (moleButton == arg0.getSource()) {
+			frame.dispose();
+			frame.remove(panel);
+			panel = new JPanel();
+			start();
+		}
+		else {
+			System.out.println("missed");
+	}
+}
+}
---
+++ src/_11_whack_a_mole/WackAMole_Runner.java
@@ -0,0 +1,8 @@
+package _11_whack_a_mole;
+
+public class WackAMole_Runner {
+	public static void main (String [] args) {
+		WackAMole wackAMole = new WackAMole();
+		wackAMole.start();
+	}
+}
